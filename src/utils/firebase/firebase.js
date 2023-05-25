33270723a22812670d752ea96a3815fad3abd492
sebastian9312 <96694150+sebastian9312@users.noreply.github.com>
2023-05-25 11:29:53 +0200

    add firestore

--- src/utils/firebase/firebase.js
+++ src/utils/firebase/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -16,9 +17,12 @@ const firebaseApp = initializeApp(firebaseConfig);
 
 const provider = new GoogleAuthProvider();
 
-provider.setCustomParameters({
-    prompt: "select_account"
-});
+// provider.setCustomParameters({
+//     prompt: "select_account"
+// });
 
-export const auth = getAuth();
+export const auth = getAuth(firebaseApp);
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+
+const db = getFirestore(firebaseApp);
