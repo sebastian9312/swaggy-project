82cf3e1c1cbcae1229157813a3d6635c137373a1
sebastian9312 <96694150+sebastian9312@users.noreply.github.com>
2023-07-24 17:10:56 +0200

    Update shop.jsx

--- src/routes/shop/shop.jsx
+++ src/routes/shop/shop.jsx
@@ -1,7 +1,5 @@
 import "./shop.styles.scss";
 
-// import { CategoriesContext } from "../../contexts/categories.context";
-// import { useContext } from "react";
 import { Route, Routes } from "react-router-dom";
 import CategoriesPreview from "../categories-preview/categories-preview";
 import Category from "../category/category";
@@ -17,34 +15,4 @@ const Shop = () => {
 };
 
 export default Shop;
-
-
-
-
-
-//     return (
-//         <>
-//             {
-//                 Object.keys(categoriesMap).map((title) => {
-
-//                     // console.log(title);
-//                     // console.log(categoriesMap[title]);
-
-//                     return (
-//                         <div key={title}>
-//                             <h2>{title}</h2>
-//                             <div className="products-container">
-//                                 {
-//                                     categoriesMap[title].map((product) => {
-//                                         return <ProductCard key={product.id} product={product} />
-//                                     })
-//                                 }
-//                             </div>
-//                         </div>
-//                     );
-//                 })
-//             };
-//         </>
-//     );
-// };
 
